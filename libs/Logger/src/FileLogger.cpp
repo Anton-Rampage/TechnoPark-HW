7ff8a2581fc82e204552777c894aa22bc8bf5641
Anton-Rampage <anton_ilin_1999@mail.ru>
2020-05-06 14:38:47 +0300

    add time to logger

--- libs/Logger/src/FileLogger.cpp
+++ libs/Logger/src/FileLogger.cpp
@@ -2,6 +2,9 @@
 
 #include "BaseException.h"
 
+#include <chrono>
+#include <iomanip>
+
 namespace tp {
 namespace logger {
 FileLogger::FileLogger(Level level, const std::string &filename) : BaseLogger(level), _log_file(filename) {
@@ -12,7 +15,9 @@ FileLogger::FileLogger(Level level, const std::string &filename) : BaseLogger(le
 
 void FileLogger::log(const std::string &str, Level log_level) {
     if (log_level <= level()) {
-        _log_file << level_str[static_cast<int>(log_level) - 1] << ' ' << str << std::endl;
+        auto time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+        _log_file << "[" << std::put_time(std::localtime(&time), "%X") << "] "
+                  << level_str[static_cast<int>(log_level) - 1] << ' ' << str << std::endl;
     }
 }
 
--- libs/Logger/src/StderrLogger.cpp
+++ libs/Logger/src/StderrLogger.cpp
@@ -1,5 +1,7 @@
 #include "StderrLogger.h"
 
+#include <chrono>
+#include <iomanip>
 #include <iostream>
 
 namespace tp {
@@ -10,7 +12,9 @@ void StderrLogger::flush() { std::cerr << std::flush; }
 
 void StderrLogger::log(const std::string &str, Level log_level) {
     if (log_level <= level()) {
-        std::cerr << level_str[static_cast<int>(log_level) - 1] << ' ' << str << std::endl;
+        auto time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+        std::cerr << "[" << std::put_time(std::localtime(&time), "%X") << "] "
+                  << level_str[static_cast<int>(log_level) - 1] << ' ' << str << std::endl;
     }
 }
 }  // namespace logger
